e44d8cc055acddb6c2ca8a5be82bdaae445062dd
Jakob Egger <jakob@eggerapps.at>
2020-12-21 15:44:54 +0100

    Removed some more occurrences of "Oktett"

--- Sources/NSError+ConvenienceConstructors.h
+++ Sources/NSError+ConvenienceConstructors.h
@@ -1,12 +1,5 @@
 #import <Foundation/Foundation.h>
 
-
-enum OktettError {
-    OktettErrorUDPSender = 5000,
-};
-
-
-
 @interface NSError (NSError_ConvenienceConstructors)
 
 +(void)set:(NSError**)error domain:(NSString *)domain code:(NSInteger)code format:(NSString*)format, ... NS_FORMAT_FUNCTION(4,5);
